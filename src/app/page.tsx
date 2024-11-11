3245c40a4f5071b1fb35fbb7f32636ec87fe201f
Eddy-mejia <eddy.mejia@iteesa.edu.hn>
2024-11-11 08:38:18 -0600

    Describe the changes made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://pixabay.com/photos/legs-feet-different-mixed-standing-362182/",
+  "https://pixabay.com/photos/air-bubbles-diving-underwater-blow-230014/",
+  "https://pixabay.com/photos/wild-horses-rainbow-released-feral-2239420/",
+  "https://pixabay.com/photos/chains-crane-industrial-heavy-load-919058/"
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}))
+
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-     Hello (Gallery in progress)
-    </main>
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+            </div>
+        ))}
+        </div>
+      </main>
   );
 }
